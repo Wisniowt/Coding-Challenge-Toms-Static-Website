f9eda7137ace66309ce00ef34db016333b261948
Local Developer <developer@local.dev>
2025-11-15 14:24:08 +0000

    Creating S3 Buckets For Dev and Prod

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    description = "AWS region"
+    type = string
+    default = "eu-west-1"
+}
+
+
+variable "website_name" {
+    description = "My webiste name for tagging resources"
+    type = string
+    default = "toms-site-329599628498"
+}
